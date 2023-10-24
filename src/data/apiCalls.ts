7d68d7539fb3b31a5e585bcc81efef859987edce
Arturas Tyskevicius <arturas.tyskevicius@vinted.com>
2023-10-24 15:30:40 +0300

    removed unnecessary code from apiCalls.ts file

--- src/data/apiCalls.ts
+++ src/data/apiCalls.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 import { GetUsersResponse } from "../types/responses";
 
-// MINE
-// export const getUsers = (): Promise<AxiosResponse<{ data: User[] }>> =>
-//   axios.get("https://reqres.in/api/users");
-
-// VINTED
 export const getUsers = () =>
   axios.get<GetUsersResponse>("https://reqres.in/api/users");
